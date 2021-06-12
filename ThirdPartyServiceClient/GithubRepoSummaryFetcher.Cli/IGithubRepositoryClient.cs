7ad9bd51582c2124b986c165f82eb146c282dcc0
smolchanovsky <smolchanovsky@gmail.com>
2021-06-12 14:26:25 +0300

    Add ThirdPartyServiceClient

---
+++ ThirdPartyServiceClient/GithubRepoSummaryFetcher.Cli/IGithubRepositoryClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using GithubRepoSummaryFetcher.Cli.Models;
+using NClient.Abstractions.HttpClients;
+using NClient.Annotations;
+using NClient.Annotations.Methods;
+using NClient.Annotations.Parameters;
+
+namespace GithubRepoSummaryFetcher.Cli
+{
+    [Header(name: "User-Agent", value: "GithubRepoSummaryFetcher.Cli")]
+    public interface IGithubRepositoryClient
+    {
+        [GetMethod("users/{userName}/repos")]
+        Task<HttpResponseWithError<RepositorySummary[], Error>> GetUserRepositoriesAsync([RouteParam] string userName);
+        [GetMethod("orgs/{orgName}/repos")]
+        Task<HttpResponseWithError<RepositorySummary[], Error>> GetOrgRepositoriesAsync([RouteParam] string orgName);
+    }
+}
