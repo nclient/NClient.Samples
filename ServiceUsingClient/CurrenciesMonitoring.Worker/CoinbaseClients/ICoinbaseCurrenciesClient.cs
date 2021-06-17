c385ced482e7356059ea6d80e7be09b53e2375ae
smolchanovsky <smolchanovsky@gmail.com>
2021-06-18 02:00:28 +0300

    Add ServiceUsingClient

---
+++ ServiceUsingClient/CurrenciesMonitoring.Worker/CoinbaseClients/ICoinbaseCurrenciesClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CurrenciesMonitoring.Worker.CoinbaseClients.Models;
+using NClient.Annotations;
+using NClient.Annotations.Methods;
+
+namespace CurrenciesMonitoring.Worker.CoinbaseClients
+{
+    [Path("v2/currencies")]
+    public interface ICoinbaseCurrenciesClient
+    {
+        [GetMethod]
+        Task<CurrenciesResponse> Get();
+    }
+}
